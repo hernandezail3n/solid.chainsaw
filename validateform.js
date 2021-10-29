cff3e4f3fae410a887cd222a5a3189da55be6de5
hernandezail3n <93409579+hernandezail3n@users.noreply.github.com>
2021-10-29 20:13:05 -0300

    Add files via upload

---
+++ validateform.js
@@ -0,0 +1,11 @@
+function validateForm() {
+    let x = document.forms["myForm"]["fname"].value;
+    if (x == "") {
+      alert("Name must be filled out");
+      return false;
+    }
+
+    function validEmail(email) {
+      var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(email);
+  };
